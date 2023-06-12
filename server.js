65d6568734023128a871044a8659e58bb14c5fd1
Arexan-K <94745953+ArexanK@users.noreply.github.com>
2023-06-12 14:50:42 +0200

    test

--- server.js
+++ server.js
@@ -1,19 +1,73 @@
-import express, {
-    json,
-    response
-} from "express";
-const app = express();
+import express from 'express'
+import * as dotenv from 'dotenv'
 
-app.set("view engine", "ejs");
-app.set("views", "./views");
+const url = 'https://demofdnd.simplicate.app/api/v2'
 
-app.use(express.static("public"));
 
-app.get('/', function (request, response) {
-    response.render('index')
-})
+// activate dotenv
+dotenv.config()
 
+
+const projectURL = url + '/projects/project?limit=6&authorization=' + process.env.AuthenticationKey + '&output=json'
+
+
+
+
+
+// Maak een nieuwe express app
+const app = express()
+
+
+// Stel afhandeling van formulieren in
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}))
+
+
+
+// Stel in hoe we express gebruiken
+app.set('view engine', 'ejs')
+app.set('views', './views')
+app.use(express.static('public'))
+
+// Maak een route voor de index
+app.get('/', (request, response) => {
+    fetchJson(projectURL).then((data) => {
+        let dataClone = structuredClone(data);
+
+        if (request.query.name) {
+            dataClone.results.name = dataClone.results.name.filter(function (name) {
+                return results.name.includes(request.query.name)
+            })
+        }
+
+
+
+
+
+
+
+        // console.log("hier staat de log van dataclone", dataClone)
+        response.render('index', dataClone)
+    });
+});
+
+
+
+// Stel het poortnummer in en start express
 app.set("port", process.env.PORT || 5050);
 app.listen(app.get('port'), function () {
     console.log(`application started on http://localhost:${app.get('port')}`)
 })
+
+/**
+ * Wraps the fetch api and returns the response body parsed through json
+ * @param {*} url the api endpoint to address
+ * @returns the json response from the api endpoint
+ */
+async function fetchJson(url) {
+    return await fetch(url)
+        .then((response) => response.json())
+        .catch((error) => error)
+}
